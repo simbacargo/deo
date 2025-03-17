e65cce9399779aba2194e4da1e606425092659a9
Macky <macdonaldnsaro@gmail.com>
2025-03-17 09:09:59 +0300

    added an index page

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -55,7 +55,7 @@ from django.urls import path
 from .views import admin_dashboard
 
 urlpatterns = [
-    path('dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('', admin_dashboard, name='admin_dashboard'),
 ] + urlpatterns
 
 
