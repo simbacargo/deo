4316c994eb8689d9a75b3daf7f0aa8d0ee5b0dbf
Developer <you@example.com>
2025-05-27 12:02:03 -0700

    Fixed Bugs

--- backend/backend/settings/__init__.py
+++ backend/backend/settings/__init__.py
@@ -1,2 +1,9 @@
 from .local import *
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://kopaduka.co.tz",
+    "https://kopaduka.co.tz",
+    "http://www.kopaduka.co.tz",
+    "https://www.kopaduka.co.tz",
+    "https://api.kopaduka.co.tz",
+    "https://server.kopaduka.co.tz",
+]
